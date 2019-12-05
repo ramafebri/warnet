4d3402b0156a1d60a5fb56ac8b345c5907fba0c3
ramafebri <38251530+ramafebri@users.noreply.github.com>
2019-12-05 21:25:17 +0700

    nmnm

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">	
-	<title>Kostan</title>
+	<title>SI WARNET</title>
 </head>
 <body>
 		<div class="koko">	
--- registeruser.php
+++ registeruser.php
@@ -61,7 +61,7 @@
                 <input type="submit" class="btn btn-primary" value="Submit">
                 <input type="reset" class="btn btn-default" value="Reset">
             </div>
-            <p>Already have an account? <a href="loginuser.php">Login here</a>.</p>
+            <p>Already have an account? <a href="halamanloginuser.php">Login here</a>.</p>
         </form>
     </div>    
 </body>
